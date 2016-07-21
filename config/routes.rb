f630016612ef1cc84ade87c004cf73c449bf3246
iolar <ikshirok@yandex.ru>
2016-07-22 03:39:20 +0700

    Authorized users can now leave comments on tickets

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
     resources :tickets
   end
 
+  resources :tickets, only: [] do
+    resources :comments, only: [:create]
+  end
+
   resources :attachments, only: [:show, :new]
 
   # Example of regular route:
