3930ae29542954d4f37c4f85a6160a8738a1d04e
iolar <ikshirok@yandex.ru>
2016-10-12 17:17:32 +0700

    Changing Mailgun to Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,19 +77,20 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Heroku Mailgun configuration
+  # Heroku Sendgrid configuration
 
   ActionMailer::Base.delivery_method = :smtp
-
+  ActionMailer::Base.raise_delivery_errors = true
   host = 'ticketee-iolar.herokuapp.com'
 
   ActionMailer::Base.smtp_settings = {
-      port:           ENV['MAILGUN_SMTP_PORT'],
-      address:        ENV['MAILGUN_SMTP_SERVER'],
-      user_name:      ENV['MAILGUN_SMTP_LOGIN'],
-      password:       ENV['MAILGUN_SMTP_PASSWORD'],
-      domain:         host,
-      authentication: :plain
+      port:                 '587',
+      address:              'smtp.sendgrid.net',
+      user_name:            ENV['SENDGRID_PASSWORD'],
+      password:             ENV['SENDGRID_USERNAME'],
+      domain:               'heroku.com',
+      authentication:       :plain,
+      enable_starttls_auto: :true
   }
 
   config.action_mailer.default_url_options = {
