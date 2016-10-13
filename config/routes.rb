87c1d63c43230d3ce93a6fc6e876c00283738e24
iolar <ikshirok@yandex.ru>
2016-10-13 20:45:18 +0700

    Added basic Tickets API

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,12 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :api do
+    resources :projects, only: [] do
+      resources :tickets
+    end
+  end
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
